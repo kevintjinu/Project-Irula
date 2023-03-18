2c38716fbfac750e8ca854a68ef32cf2042dd571
kevintjinu <52851291+kevintjinu@users.noreply.github.com>
2023-03-18 16:00:10 +0530

    frontend updated

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Modal,
   TextInput,
+  SafeAreaView,
 } from "react-native";
 import axios from "axios";
 import { Ionicons } from "@expo/vector-icons";
@@ -56,7 +57,7 @@ const Irula = () => {
     setIsFocused(true);
   };
   const handleClearSearch = () => {
-    setSearchText('');
+    setSearchText("");
     setIsFocused(false);
     setFilteredData(data);
   };
@@ -96,7 +97,17 @@ const Irula = () => {
   //     }
   //   }
   // };
-
+  // const Separator = () => {
+  //   return (
+  //     <View
+  //       style={{
+  //         height: 1,
+  //         width: '100%',
+  //         backgroundColor: 'black',
+  //       }}
+  //     />
+  //   );
+  // };
   const renderItem = ({ item }) => (
     <TouchableOpacity
       style={{
@@ -104,24 +115,48 @@ const Irula = () => {
         flexDirection: "row",
         padding: 16,
         alignItems: "center",
+        marginTop: 30,
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 15,
       }}
       onPress={() => {
         setSelectedItem(item);
         setModalVisible(true);
       }}
     >
+      <Image
+        source={require("./assets/icon.png")}
+        style={{ width: 95, height: 78 }}
+      />
       {/* <Image source={{ uri: item.urlToImage }} style={{ width: 50, height: 50, borderRadius: 25, marginRight: 16 }} /> */}
       <View style={{ flex: 1, justifyContent: "center", marginLeft: 5 }}>
-        <Text style={{ fontSize: 18, color: "red", marginBottom: 15 }}>
+        <Text
+          style={{
+            fontSize: 15,
+            color: "#284387",
+            marginBottom: 7,
+            fontWeight: "bold",
+          }}
+        >
           {item.word}
         </Text>
-        <Text style={{ fontSize: 18, color: "red", marginBottom: 15 }}>
+        <Text style={{ fontSize: 12, color: "green", marginBottom: 7 }}>
           {item.tamilword}
         </Text>
-        <Text style={{ fontSize: 12, color: "green", marginBottom: 15 }}>
+        <Text
+          numberOfLines={2}
+          ellipsizeMode="tail"
+          style={{ fontSize: 10, color: "#284387", marginBottom: 7 }}
+        >
           {item.endefinition}
         </Text>
-        <Text style={{ fontSize: 12, color: "green", marginBottom: 15 }}>
+        <Text
+          numberOfLines={2}
+          ellipsizeMode="tail"
+          style={{ fontSize: 10, color: "green", marginBottom: 7 }}
+        >
           {item.tamildefinition}
         </Text>
       </View>
@@ -133,62 +168,231 @@ const Irula = () => {
   }
 
   return (
-    <View style={styles.MainContainer}>
-      <StatusBar style="auto" />
-      <View style={styles.searchContainer}>
-        <TextInput
-          style={styles.searchInput}
-          value={searchText}
-          onChangeText={handleSearch}
-          onFocus={() => setIsFocused(true)}
-  onBlur={() => setIsFocused(false)}
-          placeholder="Search words"
-        />
-        {isFocused && (
-  <TouchableOpacity onPress={handleClearSearch}>
-    <Ionicons name="close" size={24} color="black" />
-  </TouchableOpacity>
-)}
-        <TouchableOpacity onPress={handleSearch}>
-          <Ionicons name="search" size={24} color="black" />
-        </TouchableOpacity>
-        {/* <TouchableOpacity onPress={handleVoiceSearch}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.MainContainer}>
+        <StatusBar style="auto" />
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput}
+            value={searchText}
+            onChangeText={handleSearch}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            placeholder="Search..."
+          />
+          {isFocused && (
+            <TouchableOpacity onPress={handleClearSearch}>
+              <Ionicons name="close" size={24} color="black" />
+            </TouchableOpacity>
+          )}
+          <TouchableOpacity onPress={handleSearch}>
+            <Ionicons name="search" size={24} color="black" />
+          </TouchableOpacity>
+          {/* <TouchableOpacity onPress={handleVoiceSearch}>
           <Ionicons name="mic" size={24} color="black" />
         </TouchableOpacity> */}
-      </View>
-
-      <FlatList
-        data={filteredData}
-        renderItem={renderItem}
-        keyExtractor={(item, index) => index.toString()}
-      />
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-          <Text style={{ fontSize: 24 }}>
-            {selectedItem ? selectedItem.tamildefinition : ""}
-          </Text>
-          <TouchableOpacity onPress={() => setModalVisible(false)}>
-            <Text style={{ color: "blue", marginTop: 16 }}>Close Modal</Text>
-          </TouchableOpacity>
         </View>
-      </Modal>
-    </View>
+        <View style={styles.flatlistContainer}>
+          <FlatList
+            data={filteredData}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            // ItemSeparatorComponent={Separator}
+          />
+        </View>
+
+        <Modal
+          visible={modalVisible}
+          transparent={true}
+          animationType="slide"
+          onRequestClose={() => setModalVisible(false)}
+          backdropOpacity={0.3}
+        >
+          <View style={{ flex: 1, backgroundColor: "#000000aa"}}>
+            <View style={{ flex: 1, backgroundColor: "transparent" }} />
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                marginBottom: 61,
+              }}
+            >
+              <TouchableOpacity onPress={() => setModalVisible(false)}>
+                <Ionicons name="close-circle-outline" size={70} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                height: "55%",
+                backgroundColor: "#284387",
+                borderTopLeftRadius: 20,
+                borderTopRightRadius: 20,
+                paddingHorizontal: 25,
+                paddingVertical: 15,
+              }}
+            >
+              <View
+                style={{
+                  width: "100%",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginTop: 20,
+                }}
+              >
+                <View
+                  style={{
+                    width: "48%",
+                    borderRadius: 8,
+                    backgroundColor: "#FFF",
+                    
+                  }}
+                >
+                  <View
+                    style={{
+                      borderWidth: 1,
+                      borderColor: "#FFF",
+                      borderRadius: 8,
+                      padding: 5,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "green",
+                        fontSize: 16,
+                        fontWeight:'bold',textAlign:'center'
+                      }}
+                    >
+                      {selectedItem ? selectedItem.tamilword : ""}
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    width: "48%",
+                    borderRadius: 8,
+                    backgroundColor: "#FFF",
+                    
+                    marginLeft: "4%",
+                  }}
+                >
+                  <View
+                    style={{
+                      borderWidth: 1,
+                      borderColor: "#FFF",
+                      borderRadius: 8,
+                      padding: 5,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#284387",
+                        fontSize: 16,fontWeight:'bold',textAlign:'center'
+                      }}
+                    >
+                      {selectedItem ? selectedItem.word : ""}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+<View
+style={{
+                  // width: 310,
+                  // height: 238,
+                  flexDirection: "row",
+                  // paddingHorizontal: 25,
+                  // padding: 10,
+                  justifyContent: "space-evenly",
+                  // alignItems: "center",
+                  marginTop: 23,
+                }}>
+                <View style={{flex:1,flexDirection:'column',justifyContent: "space-between",}}>
+                  
+               
+                <View
+                    style={{
+                      borderWidth: 1,
+                      borderColor: "#FFF",
+                      borderRadius: 8,
+                      padding: 5,
+                      width:"78%",backgroundColor: "#FFF",paddingVertical:10
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "green",
+                        fontSize: 14,
+                      }}
+                    >
+                      {selectedItem ? selectedItem.tamildefinition : ""}
+                    </Text>
+                    
+                  </View>
+
+
+                  <View
+                    style={{
+                      borderWidth: 1,
+                      borderColor: "#FFF",
+                      borderRadius: 8,
+                      padding: 5,
+                      marginTop: 10,width:"78%",backgroundColor: "#FFF",
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: "#284387",
+                        fontSize: 14,
+                      }}
+                    >
+                      {selectedItem ? selectedItem.endefinition : ""}
+                    </Text>
+                    
+                  </View>
+
+                  </View>
+                {/* <View style={{ width: 10, backgroundColor:'green'}} /> */}
+                <View
+                  style={{
+                    width: 128,
+                    height: 238,
+                    borderRadius: 8,
+                  //  backgroundColor:'green',
+                    // overflow: "hidden" ,
+                  }}
+                >
+                  <Image
+                    style={{ width: 128, height: 238, borderRadius: 8 }}
+                    source={require("./assets/Picture.png")}
+                  />
+                </View>
+</View>
+<TouchableOpacity style={{ flex:1,
+    marginTop: 20 }}>
+    <Text style={{ 
+      fontSize: 24,
+      padding: 10,
+      borderWidth: 2,
+      borderColor: "white",
+      backgroundColor: "#4B639D",
+      color: "white",
+      borderRadius: 10,textAlign:'center'
+    }}>
+        Hear this word 
+    </Text>
+  </TouchableOpacity>
+
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   MainContainer: {
     flex: 1,
-    backgroundColor: "#F5FCFF",
-    marginTop: 50,
-    marginBottom: 50,
-    marginHorizontal: 25,
+    backgroundColor: "#284387",
   },
 
   button: {
@@ -214,10 +418,19 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     marginHorizontal: 20,
     paddingHorizontal: 10,
+    // width:310,
+    height: 42,
   },
   searchInput: {
     flex: 1,
     fontSize: 18,
+  },
+  flatlistContainer: {
+    flex: 1,
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 8,
+    margin: 24,
   },
 });
 
